813e6fd0568fde8b54d6fa6be96189b661b4c19b
Austin Keeley <austin.keeley@gmail.com>
2016-05-07 12:22:14 -0400

    Adding client code for getting openvpn instances

---
+++ README.md
@@ -0,0 +1,7 @@
+# pfsense-openvpn
+
+Gets VPN and user information for OpenVPN instances hosted by a pfSense host.
+
+# Installation
+
+    npm install pfsense-openvpn
--- client.js
+++ client.js
@@ -1,7 +1,47 @@
+/* client.js */
+
+var request = require('request');
+var cheerio = require('cheerio');
+
 /**
  * Gets all the OpenVPN instances running on this pfSense host
  */
 var getInstances = function(session, cb) {
+  request.get({
+    url: 'http://' + session.hostname + '/vpn_openvpn_server.php',
+    headers: {
+      'Cookie': 'PHPSESSID=' + session.token
+    }
+  }, function(err, response) {
+    if (err) { cb(err, null); return; }
+
+    var $ = cheerio.load(response.body);
+    var results = [];
+    var id = 0;
+    $('.sortable > tbody > tr').map(function(index, row) {
+      // This table has a hidden row that we can just ignore.
+      var display = $(row).css('display');
+      if (display === "none") { return null; }
+
+      var cols = [];
+      $(row).find('td').each(function(index, col) {
+          cols.push($(col).text());
+      });
+      var disabled = cols[0].trim();
+      var protocolPort = cols[1].trim();
+      var network = cols[2].trim();
+
+      results.push({
+        id: id++,
+        enabled: disabled === 'NO',
+        protocol: protocolPort.split('/')[0].trim(),
+        port: protocolPort.split('/')[1].trim(),
+        network: network
+      });
+    });
+    cb(null, results);
+
+  });
 };
 
 /**
