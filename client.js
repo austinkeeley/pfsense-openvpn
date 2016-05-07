916307c4b7a29348148473659edfe9bdedaa8b65
Austin Keeley <austin.keeley@gmail.com>
2016-05-06 22:17:42 -0400

    initial commit

---
+++ client.js
@@ -0,0 +1,16 @@
+/**
+ * Gets all the OpenVPN instances running on this pfSense host
+ */
+var getInstances = function(session, cb) {
+};
+
+/**
+ * Gets all the users connected to an OpenVPN instance
+ */
+var getUsers = function(session, instance, cb) {
+};
+
+module.exports = {
+  getInstances: getInstances,
+  getUsers: getUsers
+};
